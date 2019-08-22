f8f30b94d5d39e944b63532cb62931d0784fcace
Ning Bei <beiningshell@outlook.com>
2019-08-22 17:36:03 +0100

    update

--- Code/bat_v2_zone/SCRIPT_RunSim17.m
+++ Code/bat_v2_zone/SCRIPT_RunSim17.m
@@ -3,183 +3,136 @@ clear
 close all
 
 RunExperiments = 6;
+plot_R = 0;
 
 for ExperimentNr = RunExperiments
     
-    settings.delay_window = 0.001;
-    settings.linear_velocity = 6;
-    settings.fovea = 90;
-    settings.doplot=0;
-    settings.attenuation_range = [-10 -6];
-    settings.max_slope = 30/180*pi;
-    settings.nr_replications = NaN;
     settings.emission_freq = 40000;
+    settings.nr_replications = 1;
+    settings.targetsearch = 1;
+    settings.iteration_steps = 1000;
     
-    targetsearch = true;
-    RandomPhase = 0;
-    ReflectorStrength = 0;
+    %% bat variables
+    settings.innerzone = 0.2;
+    settings.outerzone = 0.5;
+    zone_thickness = settings.outerzone - settings.innerzone;
+    settings.delay_window = 0.001;
+    settings.call_freq = 0.1;
+    settings.linear_vel_max = 6;
+    settings.linear_vel_min = 0.3;
+    settings.angular_vel_max = 666;
+    settings.angular_vel_min = 10;
     
+    %% environment factors
+    settings.rand_phase      = 0;
+    settings.reflector_str   = 0;
+    settings.attenuation_range = [-10 -6];
     
+    %% environment set up
     switch ExperimentNr
         
         % 3D tunnel / ring
         case 6
-            if isnan(settings.nr_replications);settings.nr_replications=1;end
-            settings.iteration_steps = 1000;
-            settings.worldshape = '3D';  % 'H' for 2D and '3D' for 3D
-            ResultFile = 'Experiment6';
+            settings.worldshape = '3D'; 
+            ResultFile = '3Dtunnel';
             
-            Rring = 5;                              % radius of ring
-            x0 = 5; y0 = 0; z0 = 0;                 % center of ring
-            Rtorus = 2;                             % radius of torus
-            R1 = torus(Rring,Rtorus,x0,y0,z0);
-            
+            settings.Rring = 5;                                 % radius of ring
+            settings.x0 = 5; settings.y0 = 0; settings.z0 = 0;  % center of ring
+            settings.Rtorus = 2;                                % radius of torus
+            settings.R = torus(settings.Rring,settings.Rtorus,settings.x0,settings.y0,settings.z0);
             %             [x,y,z] = ndgrid(4.5:0.1:5.5, -0.5:0.1:0.5, 3:0.1:3.5);
             %             R2 = [x(:),y(:),z(:)];
-            %
             %             R = [R1;R2];
+%             settings.R = R1;
             
-            R = R1;
+            settings.target = [10,0,0.5];
+            linear_distance = norm(settings.target);
             
-            target = [10,0,-2];
-            
-            
-            % 2d vertical wires
-        case 1
-            if isnan(settings.nr_replications);settings.nr_replications=1;end
-            settings.iteration_steps = 300;
-            settings.worldshape = 'H';
-            ResultFile = 'Experiment1';
-            
-            [x,y,z] = ndgrid(-10:0.3:10, 0, -10:0.3:10);
-            R = [x(:),y(:),z(:)];
-            target = [5 0 5];
-            
-            
-            % 2d corridor
-        case 2
-            if isnan(settings.nr_replications);settings.nr_replications=1;end
-            settings.iteration_steps = 100;
-            settings.worldshape = 'H';
-            ResultFile = 'Experiment2';
-            
-            [x,y,z] = ndgrid(-0.5:1:0.5, 0, -10:0.01:10);
-            R = [x(:),y(:),z(:)];
-            target = [0, 0, -5];
-            
-            
-            % 2d Circular corridor
-        case 3
-            if isnan(settings.nr_replications);settings.nr_replications=1;end
-            settings.iteration_steps = 300;
-            settings.worldshape = 'H';
-            ResultFile = 'Experiment3';
-            
-            r1 = 4;
-            r2 = 5.4;
-            range = (0:0.001:2*pi)';
-            x1 = 5 + r1 * cos(range);
-            z1 = r1 * sin(range);
-            y1 = zeros(length(range),1);
-            x2 = 5 + r2 * cos(range);
-            z2 = r2 * sin(range);
-            y2 = zeros(length(range),1);
-            
-            R1 = [x1(:), y1(:), z1(:)];
-            R2 = [x2(:), y2(:), z2(:)];
-            R = [R1;R2];
-            
-            target = [9.5 0 0];
-            
-            
-            % 3D wires
+        % 3D wires
         case 4
-            if isnan(settings.nr_replications);settings.nr_replications=1;end
-            settings.iteration_steps = 300;
             settings.worldshape = '3D';
-            ResultFile = 'Experiment4';
+            ResultFile = '3Dwires';
             
-            [x,y,z] = ndgrid(-6.3:1:6, -8:0.1:8, -8.8:1:14);
-            R = [x(:),y(:),z(:)];
+            [x,y,z] = ndgrid(-0.3:1:20, -8:0.1:8, -0.8:1:20);
+            settings.R = [x(:),y(:),z(:)];
             
-            target = [2,-1,10];
+            settings.target = [16,0,18];
+            linear_distance = norm(settings.target);
             
             % 3d clusters
         case 5
-            if isnan(settings.nr_replications);settings.nr_replications=1;end
-            settings.iteration_steps = 200;
             settings.worldshape = '3D';
-            ResultFile = 'Experiment5';
+            ResultFile = '3Dclusters';
             
-            cluster_nr = 200;
-            min = -20;
-            max = 20;
-            xcenter = randrange(min,max,cluster_nr);
-            ycenter = randrange(-20,20,cluster_nr);
-            zcenter = randrange(min,max,cluster_nr);
+            settings.cluster_nr = 50;
+            settings.centers_min = 0;
+            settings.centers_max = 20;
+            xcenter = randrange(settings.centers_min,settings.centers_max,settings.cluster_nr);
+            ycenter = randrange(-5,5,settings.cluster_nr);
+            zcenter = randrange(settings.centers_min,settings.centers_max,settings.cluster_nr);
             
-            cluster_range = 1;
-            ref_nr_per_cluster = 300;
+            settings.cluster_range = 1;
+            settings.ref_nr_per_cluster = 1000;
             x = [];
             y = [];
             z = [];
             
-            for i = 1:1:cluster_nr
-                xf = randn(ref_nr_per_cluster, cluster_range) + xcenter(i);
-                yf = randn(ref_nr_per_cluster, cluster_range) + ycenter(i);
-                zf = randn(ref_nr_per_cluster, cluster_range) + zcenter(i);
+            for i = 1:1:settings.cluster_nr
+                xf = randn(settings.ref_nr_per_cluster, settings.cluster_range) + xcenter(i);
+                yf = randn(settings.ref_nr_per_cluster, settings.cluster_range) + ycenter(i);
+                zf = randn(settings.ref_nr_per_cluster, settings.cluster_range) + zcenter(i);
                 x = [x;xf];
                 y = [y;yf];
                 z = [z;zf];
             end
             
-            R = [x(:), y(:), z(:)];
+            settings.R = [x(:), y(:), z(:)];
+            settings.target = [15,0,15];
+            linear_distance = norm(settings.target);
             
-            target = [8,0,8];
-            
+          
     end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
     distances           = NaN(settings.iteration_steps, 1,  settings.nr_replications);
     velocities          = NaN(settings.iteration_steps, 1,  settings.nr_replications);
-    handles             = NaN(settings.iteration_steps, 1,  settings.nr_replications);
-    densities           = NaN(settings.iteration_steps, 1,  settings.nr_replications);
+%     densities           = NaN(settings.iteration_steps, 1,  settings.nr_replications);
     batpositions        = NaN(settings.iteration_steps, 3,  settings.nr_replications);
     reflectors_nr       = NaN(settings.iteration_steps, 4,  settings.nr_replications);
-    steermats           = NaN(settings.iteration_steps, 4,  settings.nr_replications);
-    iteration_times     = NaN(1,settings.nr_replications);
+%     steermats           = NaN(settings.iteration_steps, 4,  settings.nr_replications);
+    iteration_times     = NaN(settings.nr_replications,1);
+    collision_times     = NaN(settings.nr_replications,1);
+    tortuosity          = NaN(settings.nr_replications,1);
     
+    
+    %% run simulation
     for replication = 1:settings.nr_replications
         
         currentsettings = settings;
-        currentsettings.reflectors = R;
-        currentsettings.rand_phase = RandomPhase;
-        currentsettings.reflector_str = ReflectorStrength;
-        currentsettings.target = target;
-        currentsettings.targetsearch = targetsearch;
-        
         output = batflight25(currentsettings);
         
         distances(:,:,replication)      = output.objdistlog;
         velocities(:,:,replication)     = output.velocities;
-        handles(:,:,replication)        = output.handlelog;
+%         handles(:,:,replication)        = output.handlelog;
         batpositions(:,:,replication)   = output.batposlog;
-        steermats(:,:,replication)      = output.steermatlog;
+%         steermats(:,:,replication)      = output.steermatlog;
         reflectors_nr(:,:,replication)  = output.reflectors_nr;
-        iteration_times(:,replication)  = output.iteration_times;
+        iteration_times(:,1)            = output.iteration_times;
+        collision_times(:,1)            = output.collision;
+        tortuosity(:,1)                 = output.tortuosity;
     end
     
-    save(['experiments\\' ResultFile]);
-    %subject = ['Experiment ' num2str(ExperimentNr) ' finished'];
-    %SendMailToUA(subject,'Simulation finished!')
+    %% save data
+    save(ResultFile);
     
     
-    % plot bat_position and reflectors
+    %% plot bat_position and reflectors
     figure (1)
     set(1,'position',[500 300 900 700])
     plot3(batpositions(:,1), batpositions(:,2), batpositions(:,3), '.r')
     hold on
-    %     plot3(R(:,1),R(:,2),R(:,3),'.b')
+    if plot_R == 1
+        plot3(settings.R(:,1),settings.R(:,2),settings.R(:,3),'.b')
+    end
     %     plot3(R2(:,1),R2(:,2),R2(:,3),'.b')
     axis equal
     %     axis auto
@@ -189,8 +142,8 @@ for ExperimentNr = RunExperiments
     xlabel('X')
     ylabel('Y')
     zlabel('Z')
-    if targetsearch == true
-        text(target(1),target(2),target(3),'Target')
+    if settings.targetsearch == 1
+        text(settings.target(1),settings.target(2),settings.target(3),'Target')
     end
     text(0,0,0,'Start')
     grid on
--- Code/bat_v2_zone/batflight25.m
+++ Code/bat_v2_zone/batflight25.m
@@ -2,16 +2,17 @@ function output = batflight25(settings)
 
 delay_window = settings.delay_window;
 iteration_steps = settings.iteration_steps;
-reflectors = settings.reflectors;
+reflectors = settings.R;
 worldshape = settings.worldshape;
-max_vel = settings.linear_velocity;
-steermatlog = NaN(iteration_steps,4);
+max_vel = settings.linear_vel_max;
+min_vel = settings.linear_vel_min;
+an_max_vel = settings.angular_vel_max;
+an_min_vel = settings.angular_vel_min;
+% steermatlog = NaN(iteration_steps,4);
 batposlog=NaN(iteration_steps,3);
 objdistlog = NaN(iteration_steps,1);
-handlelog = NaN(iteration_steps,1);
-ipi_log = NaN(iteration_steps,1);
-rotation_log_az =NaN(iteration_steps,3);
-rotation_log_el =NaN(iteration_steps,3);
+% rotation_log_az =NaN(iteration_steps,3);
+% rotation_log_el =NaN(iteration_steps,3);
 velocities = NaN(iteration_steps,1);
 reflectors_nr_Log = NaN(iteration_steps,4);
 target = settings.target;
@@ -19,21 +20,22 @@ targetsearch = settings.targetsearch;
 rand_phase = settings.rand_phase;
 reflector_str = settings.reflector_str;
 attenuation_range = settings.attenuation_range;
+inner_zone = settings.innerzone;
+outer_zone = settings.outerzone;
+ipi = settings.call_freq;
 
 X=reflectors(:,1);Y=reflectors(:,2);Z=reflectors(:,3);
 
 bat_pos_ini = [0 0 0];
 bat_pos = bat_pos_ini;
+
 heading = [0;0;1];
 X_axis = [1;0;0];
 Y_axis = [0;1;0];
 
-inner_zone = 0.2;
-outer_zone = 0.4;
-
-iteration_times = 0;
 collision = 0;
 reaction_time = 50/1000;
+tortuosity = 0;
 
 for iteration = 1:iteration_steps
     
@@ -63,17 +65,15 @@ for iteration = 1:iteration_steps
     if isnan(objdist);objdist=1000;end
     closest_distance = min(objdist);
     
-    %set new timescale, magnitude and lineair velocity
-    ipi = 0.1;
     
     %% calculate the desired velocity
-    linear_velocity = interp1([2000 5 0],[max_vel max_vel 0.3],closest_distance);
+    linear_velocity = interp1([20000 5 0],[max_vel max_vel min_vel],closest_distance);
     if isnan(linear_velocity);linear_velocity=max_vel;end
     
     %%%% angular velocity related to linear velocity
-    %     magnitude = interp1([0 10],[666 -500],linear_velocity);
-    magnitude = interp1([0 6],[666 10],linear_velocity);
-    if magnitude<0;magnitude=0;end
+    %     angular_vel = interp1([0 10],[666 -500],linear_velocity);
+    angular_vel = interp1([0 max_vel],[an_max_vel an_min_vel],linear_velocity);
+    if angular_vel<0;angular_vel=0;end
     
     %set rotation time and processing time
     processing_time = 2*(closest_distance/343) + reaction_time + delay_window;
@@ -110,8 +110,8 @@ for iteration = 1:iteration_steps
     current_el_sign = options(2,minindexTB);
     
     % scale azimuth and elevation rotation with rotation time
-    current_az=deg2rad(current_az_sign*magnitude*rotation_time);
-    current_el=deg2rad(current_el_sign*magnitude*rotation_time);
+    current_az=deg2rad(current_az_sign*angular_vel*rotation_time);
+    current_el=deg2rad(current_el_sign*angular_vel*rotation_time);
     
     az_temp = current_az;
     el_temp = current_el;
@@ -130,12 +130,10 @@ for iteration = 1:iteration_steps
     
     if target_distance <= 0.15
         break;
-    else
-        iteration_times = iteration_times + 1;
     end
     
     %% target search / prefered height
-    if targetsearch == true
+    if targetsearch == 1
         
         % outside the zone
         if closest_distance > outer_zone
@@ -149,7 +147,8 @@ for iteration = 1:iteration_steps
             % Compare heading and target vector
             delta_az = target_az - bat_az;
             delta_el = 0.35*(target_el - bat_el);
-            %             delta_el = (log10(target_el - bat_el + 1))*(target_el - bat_el);
+%             delta_el = ((1/a)^abs(target_el - bat_el)) *(target_el - bat_el);
+%             delta_el = (-0.07*(target_el - bat_el) +0.2) *(target_el - bat_el);
             
             % constrain delta_az between -pi to pi
             % no need for delta_el, because delta_el is not angle
@@ -164,99 +163,90 @@ for iteration = 1:iteration_steps
             % if can't (delta is bigger than max_angle), delta = max_angle
             delta_az_abs = abs(delta_az);
             delta_el_abs = abs(delta_el);
-            Max_angle_az = deg2rad(magnitude * rotation_time);
+            Max_angle = deg2rad(angular_vel * rotation_time);
             %             Max_angle_el = (1/2.5)^abs(target_el - bat_el);
             
-            if delta_az_abs < Max_angle_az
+            if delta_az_abs < Max_angle
                 current_az = delta_az;
             else
-                current_az = sign(delta_az) * Max_angle_az;
+                current_az = sign(delta_az) * Max_angle;
             end
-            if delta_el_abs < Max_angle_az
+            if delta_el_abs < Max_angle
                 current_el = delta_el;
             else
-                current_el = sign(delta_el) * Max_angle_az;
+                current_el = sign(delta_el) * Max_angle;
             end
+         
             
-            % inside the zone
+            
+        % inside the zone
         elseif closest_distance >= inner_zone && closest_distance <= outer_zone
             
-            % Calculate target vector and the desired turning angle(az and el)
             target_vector = target - bat_pos;
             target_az = atan2(target_vector(3), target_vector(1));  % target position
             bat_az = atan2(heading(3),heading(1));                  % bat position
             target_el = target_vector(2);                           % target height
             bat_el = heading(2);                                    % bat height
-            
-            % Compare heading and target vector
             delta_az = target_az - bat_az;
             delta_el = 0.35*(target_el - bat_el);
-            %             delta_el = (log10(target_el - bat_el + 1))*(target_el - bat_el);
+%             delta_el = ((1/a)^abs(target_el - bat_el)) *(target_el - bat_el);
+%             delta_el = (-0.07*(target_el - bat_el) +0.2) *(target_el - bat_el);
             
-            % constrain delta_az between -pi to pi
-            % no need for delta_el, because delta_el is not angle
-            % delta_el is similar to a PID controller
             if delta_az > pi
                 delta_az = -2*pi + delta_az;
             elseif delta_az < -pi
                 delta_az = 2*pi + delta_az;
             end
             
-            % Determine if delta angle can be achieved in this iteration
-            % if can't (delta is bigger than max_angle), delta = max_angle
             delta_az_abs = abs(delta_az);
             delta_el_abs = abs(delta_el);
-            Max_angle_az = deg2rad(magnitude * rotation_time);
-            %             Max_angle_el = (1/2.5)^abs(target_el - bat_el);
+            Max_angle = deg2rad(angular_vel * rotation_time);
             
-            if delta_az_abs < Max_angle_az
+            if delta_az_abs < Max_angle
                 current_az = delta_az;
             else
-                current_az = sign(delta_az) * Max_angle_az;
+                current_az = sign(delta_az) * Max_angle;
             end
-            if delta_el_abs < Max_angle_az
+            if delta_el_abs < Max_angle
                 current_el = delta_el;
             else
-                current_el = sign(delta_el) * Max_angle_az;
+                current_el = sign(delta_el) * Max_angle;
             end
             
             m = (closest_distance-inner_zone)/(outer_zone-inner_zone);
             current_az = m * current_az + (1-m) * az_temp;
             current_el = m * current_el + (1-m) * el_temp;
             
-            
         end
     end
     
     
     %% constrain on elevation
-    
     if strcmp(worldshape,'H');current_el=0;end
-    
     
     %% move world--during processing and wait
     movement = linear_velocity * processing_time;
     [X,Y,Z] = worldflow([X,Y,Z], 0, 0, movement);
-    bat_pos = bat_pos + heading' * movement;
+    bat_pos_temp = bat_pos + heading' * movement;
+    tortuosity = tortuosity + norm(bat_pos_temp - bat_pos);
+    bat_pos = bat_pos_temp;
     
     %% move world--during rotation time
-    
-    % move to the when current_el is determined
     movement = linear_velocity * rotation_time;
     [X,Y,Z] = worldflow([X,Y,Z], current_az, current_el, movement);
     [heading,Y_axis,X_axis] = headingRot(heading,Y_axis,X_axis,current_el,current_az);
-    bat_pos = bat_pos + heading' * movement;
-    
+    bat_pos_temp = bat_pos + heading' * movement;
+    tortuosity = tortuosity + norm(bat_pos_temp - bat_pos);
+    bat_pos = bat_pos_temp;
     
     %% logging
     reflectors_nr_Log(iteration,:) = nr_reflectors;
-    steermatlog(iteration,:)    = steermat;
+%     steermatlog(iteration,:)    = steermat;
     objdistlog(iteration)       = closest_distance;
     batposlog(iteration,:)      = bat_pos;
-    ipi_log(iteration)          = ipi;
-    rotation_log_az(iteration)  = current_az;
-    rotation_log_el(iteration)  = current_el;
-    displayinfo = [iteration rad2deg(current_az) rad2deg(current_el) collision];
+%     rotation_log_az(iteration)  = current_az;
+%     rotation_log_el(iteration)  = current_el;
+    displayinfo = [iteration rad2deg(current_az) rad2deg(current_el)];
     str =sprintf('%+03.2f\t',displayinfo);
     disp(str);
     velocities(iteration) = linear_velocity;
@@ -266,16 +256,17 @@ end
 output.reflectors_nr       = reflectors_nr_Log;
 output.velocities          = velocities;
 output.batposlog           = batposlog;
-output.steermatlog         = steermatlog;
+% output.steermatlog         = steermatlog;
 output.objdistlog          = objdistlog;
-output.LEFT                = LEFT;
-output.RIGHT               = RIGHT;
-output.objrange            = objrange;
-output.objdist             = objdist;
-output.handlelog           = handlelog;
-output.ipi_log             = ipi_log;
-output.rotation_log_az     = rotation_log_az;
-output.rotation_log_el     = rotation_log_el;
-output.iteration_times     = iteration_times;
+% output.LEFT                = LEFT;
+% output.RIGHT               = RIGHT;
+% output.objrange            = objrange;
+% output.objdist             = objdist;
+% output.ipi_log             = ipi_log;
+% output.rotation_log_az     = rotation_log_az;
+% output.rotation_log_el     = rotation_log_el;
+output.iteration_times     = iteration;
+output.collision           = collision;
+output.tortuosity          = tortuosity;
 
 end
